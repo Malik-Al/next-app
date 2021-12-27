4ea418a60dbe1cbf0e425cb058a856b2f57c60b2
malik <Malik-Al>
2021-12-28 00:48:01 +0600

    init

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import MainContainer from "../componenst/MainContainer";
+
+const Index = () => {
+    return (
+                <MainContainer keywords={'main page'}>
+                    <h1>Главная страница</h1>
+                </MainContainer>
+    );
+};
+
+export default Index;
